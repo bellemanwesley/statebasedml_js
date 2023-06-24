2e1228dd608b6ba0065f96831999aa162bdc6f28
Wesley Belleman <bellemanwesley@gmail.com>
2023-06-24 18:35:41 -0400

    making some updates

---
+++ statebasedml.js
@@ -0,0 +1,13 @@
+class data {
+    constructor(datalist) {
+        this.datalist = datalist;
+    }
+    train(model) {
+        if (model===undefined) {
+            return "this is working";
+        }
+    }
+    classify() {
+
+    }
+}
